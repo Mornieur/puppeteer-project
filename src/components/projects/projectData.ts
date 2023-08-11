5cf14e736d8767793219ea7f11574385ac42f907
Mornieur <mariafernanda-coro@hotmail.com>
2023-08-11 17:34:27 -0300

    static: add project information

---
+++ src/components/projects/projectData.ts
@@ -0,0 +1,105 @@
+export const projects = [
+  {
+    id: 1,
+    name: 'Task Form',
+    description:
+      'The Task Form project is a web application that allows users to create and manage their tasks in a simple and efficient way. The idea came from the need to better organize my daily activities and, at the same time, improve my web development skills.',
+    url: 'https://task-form-ashy.vercel.app/',
+  },
+  {
+    id: 2,
+    name: 'Rick and Morty Project',
+    description:
+      'Rick and Morty card creation project from an api, using nextjs.',
+    url: 'https://rick-and-morty-api-mornieur.vercel.app/',
+  },
+  {
+    id: 3,
+    name: 'Leadster Landing Page',
+    description:
+      'landing page creation using nextjs and typescript. documenting with storybook and testing with jest. ',
+    url: 'https://leadster-landing-page-six.vercel.app/',
+  },
+  {
+    id: 4,
+    name: 'Storybook Project',
+    description:
+      'creation of loading component and buttons for documentation in the storybook.',
+    url: 'https://storybook-project.vercel.app/',
+  },
+  {
+    id: 5,
+    name: '3D iPhone Website',
+    description:
+      'creation of apple website with 3d rendering using threejs and gsap library.',
+    url: 'https://3d-iphone-website-threejs.vercel.app/',
+  },
+  {
+    id: 6,
+    name: 'Tailwind Spotify',
+    description:
+      'recreating the spotify interface using tailwind and storybook for documentation.',
+    url: 'https://tailwind-spotify-omega.vercel.app/',
+  },
+  {
+    id: 7,
+    name: 'CardAnimation',
+    description:
+      'animated cards component made in nextjs, using the framer-motion library and styling with styled-components. ',
+    url: 'https://card-animation-mornieur.vercel.app/',
+  },
+  {
+    id: 8,
+    name: 'HealthForm',
+    description:
+      'Health and Nutrition Sheet made in Nextjs and Typescript, using styled-components for styling and yup-schema for validation.',
+    url: 'https://health-form-cgtn-9y0zrfv5b-mornieur.vercel.app/',
+  },
+  {
+    id: 9,
+    name: 'Angular-TodoList',
+    description:
+      'simple to-do list made in angular with typescript and styled with bootstrap.',
+    url: 'https://todo-list-angular-d05xnc2sq-mornieur.vercel.app/',
+  },
+  {
+    id: 10,
+    name: 'Landing-Page',
+    description:
+      'Healthy food landing page developed with javascript and sass.',
+    url: 'https://pagina-de-comidas-saudaveis-9njg-971rg8t4x-mornieur.vercel.app/',
+  },
+  {
+    id: 11,
+    name: 'Pokedex',
+    description: 'API consumption to build a pokedex.',
+    url: 'https://pokedex-ahiec5yxb-mornieur.vercel.app/',
+  },
+  {
+    id: 12,
+    name: 'Twitter Responsive Clone',
+    description:
+      'This is a Twitter clone project built with React, TypeScript, and Styled-components, focusing on responsiveness to adapt seamlessly to various screen sizes.',
+    url: 'https://clone-twitter-responnsive-bwjprgvfo-mornieur.vercel.app/',
+  },
+  {
+    id: 13,
+    name: 'Piano Project',
+    description:
+      'This is a simple piano project that allows users to interact with a virtual piano using their mouse clicks. It is built with HTML, SCSS, and JavaScript.',
+    url: 'https://piano-project.vercel.app/',
+  },
+  {
+    id: 14,
+    name: 'Dev Finance',
+    description:
+      'Dev.finance is a simple finance application designed to calculate income and expenses, helping users keep track of their financial transactions. It is built using JavaScript, HTML, and CSS.',
+    url: 'https://dev-finance-sable.vercel.app/',
+  },
+  {
+    id: 15,
+    name: 'Netflix Responsive Clone',
+    description: 'Responsive Netflix clone using Reactjs, consuming Movie API.',
+    url: 'https://clone-netflix-responsive-ika0cw04j-mornieur.vercel.app/',
+  },
+];
