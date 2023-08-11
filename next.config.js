b6d965e390a3d35f2dd0a6a3b0bb8e6bdcff21ed
Mornieur <mariafernanda-coro@hotmail.com>
2023-08-11 17:12:47 -0300

    build: add project settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    domains: ['media-2.api-sports.io'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
